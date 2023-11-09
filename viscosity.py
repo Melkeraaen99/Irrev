75f9b29496786b5565d71b24193003381c5febb7
Thomas <thomawm@stud.ntnu.no>
2023-11-09 14:15:38 +0100

    Calculate entropy with entropy_tv, viscosity is now dimensionless, plot is now what we want it to be, next step is to add experimental values and compare, then check for other properties such as conductivity and diffusion

--- viscosity.py
+++ viscosity.py
@@ -6,6 +6,7 @@ from thermopack.cubic import cubic
 from thermopack.saftvrmie import saftvrmie
 from scipy.constants import Boltzmann
 from scipy.constants import R
+from scipy.constants import Avogadro
 
 # Create a MieKinGas instance
 #eos = cubic('AR', 'PR') 
@@ -15,18 +16,20 @@ kin_id = MieKinGas('AR', is_idealgas = True)
 
 # Define temperature and pressure ranges
 T_values = [200, 250, 300]  # Kelvin
-pressure_start = 1e5  # Pascal
+pressure_start = 10e5  # Pascal -> 1 bar
 pressure_end = 100e5
 pressure_increment = 5e5
 pressure = np.arange(pressure_start, pressure_end + 1, pressure_increment)
 x = [0.5, 0.5]  # Molar composition
 z = [1]
-m_Ar = 39.948 # g/mol
+m_Ar = 39.948*1.67377e-27 # kg
+molar_mass_argon = 39.948e-3 # kg/mol
 kb = Boltzmann # J/K
 
 # Store Residual entropy with thermopack and by calculating it
 entropy = {T: [] for T in T_values}
 entropy_2 = {T: [] for T in T_values}
+entro_3 = {T: [] for T in T_values}
 
 # Initialize lists to store viscosity data for different temperatures
 visc_data = {T: [] for T in T_values}
@@ -40,7 +43,7 @@ for T in T_values:
         entro, = eos.entropy(T, p, z, eos.VAPPH, residual=True) # Feil i denne beregningen da residual tror p er v
         entro_full, = eos.entropy(T, p, z, eos.VAPPH)
         entro_null, = eos.entropy(T, 0.1, z, eos.VAPPH)
-        entropy_2[T].append(entro_full-entro_null)
+        entropy_2[T].append(entro_full-entro_null) # Ikke bruk, 
         entropy[T].append(entro)
         visc = kin.viscosity_tp(T, p, x, N=2)
         visc_id = kin_id.viscosity_tp(T, 1, x, N=2)
@@ -48,6 +51,10 @@ for T in T_values:
         visc_data_id[T].append(visc_id)
         spes_volume = eos.specific_volume(T, p, z, eos.VAPPH)
         density[T].append(1/spes_volume[0])
+        entropy_3 = eos.entropy_tv(T, spes_volume[0], z, property_flag='R')
+        entro_3[T].append(entropy_3) 
+
+print(visc_data)
 
 # Scaled viscosity
 def dim_less_visc(T): 
@@ -55,9 +62,9 @@ def dim_less_visc(T):
 
 def dim_less_visc_2(T):
     if T in density:
-        return np.array(density[T]) ** (-2/3) * ((R * T) / m_Ar) ** (-0.5) * (1 / m_Ar) * np.array(visc_data[T])
+        return np.log((np.array(density[T])*Avogadro)**(-2/3) * (kb * T* m_Ar)**(-0.5) * np.array(visc_data[T]))
     else:
-        return None  # Handle the case where T is not in the density dictionary
+        return None 
 
 
 # Create a new figure
@@ -71,7 +78,7 @@ plt.figure(figsize=(8, 6))
 # Plot calculated residual entropy with log scaled viscosity, try different scaling
 for T in T_values:
     label = f'Temperature {T} K (Ideal Gas)'
-    plt.plot(entropy_2[T], dim_less_visc_2(T), 'o', label=label)
+    plt.plot(entro_3[T], dim_less_visc_2(T), 'o', label=label)
 
 # Add labels and a legend
 plt.xlabel(r'Residual entropy [J·mol$^{-1}$·K$^{-1}$]')
