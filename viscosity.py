ea0e530d0abca89e0b7de74f72b1fcd73cf2daae
Thomas <thomawm@stud.ntnu.no>
2023-11-09 14:31:32 +0100

    Cleaned up everything we don't use so that the file is easier to understand

--- viscosity.py
+++ viscosity.py
@@ -12,7 +12,6 @@ from scipy.constants import Avogadro
 #eos = cubic('AR', 'PR') 
 eos = saftvrmie('AR') 
 kin = MieKinGas('AR', use_eos=eos)  # AR, modeled with RET-Mie
-kin_id = MieKinGas('AR', is_idealgas = True)
 
 # Define temperature and pressure ranges
 T_values = [200, 250, 300]  # Kelvin
@@ -28,39 +27,25 @@ kb = Boltzmann # J/K
 
 # Store Residual entropy with thermopack and by calculating it
 entropy = {T: [] for T in T_values}
-entropy_2 = {T: [] for T in T_values}
-entro_3 = {T: [] for T in T_values}
 
 # Initialize lists to store viscosity data for different temperatures
 visc_data = {T: [] for T in T_values}
-visc_data_id = {T: [] for T in T_values}
 
 # Density
 density = {T: [] for T in T_values}
 
 for T in T_values:
     for p in pressure:
-        entro, = eos.entropy(T, p, z, eos.VAPPH, residual=True) # Feil i denne beregningen da residual tror p er v
-        entro_full, = eos.entropy(T, p, z, eos.VAPPH)
-        entro_null, = eos.entropy(T, 0.1, z, eos.VAPPH)
-        entropy_2[T].append(entro_full-entro_null) # Ikke bruk, 
-        entropy[T].append(entro)
         visc = kin.viscosity_tp(T, p, x, N=2)
-        visc_id = kin_id.viscosity_tp(T, 1, x, N=2)
         visc_data[T].append(visc)
-        visc_data_id[T].append(visc_id)
         spes_volume = eos.specific_volume(T, p, z, eos.VAPPH)
         density[T].append(1/spes_volume[0])
-        entropy_3 = eos.entropy_tv(T, spes_volume[0], z, property_flag='R')
-        entro_3[T].append(entropy_3) 
+        entro = eos.entropy_tv(T, spes_volume[0], z, property_flag='R')
+        entropy[T].append(entro) 
 
-print(visc_data)
 
 # Scaled viscosity
-def dim_less_visc(T): 
-    return np.log(np.array(visc_data[T])/visc_data_id[T])
-
-def dim_less_visc_2(T):
+def dim_less_visc(T):
     if T in density:
         return np.log((np.array(density[T])*Avogadro)**(-2/3) * (kb * T* m_Ar)**(-0.5) * np.array(visc_data[T]))
     else:
@@ -70,20 +55,15 @@ def dim_less_visc_2(T):
 # Create a new figure
 plt.figure(figsize=(8, 6))
 
-# Plot Residual entropy with resiudal return value from thermopack
-'''for T in T_values:
-    label = f'Temperature {T} K (Non-Ideal Gas)'
-    plt.plot(entropy[T], dim_less_visc, 'x', label=label)'''
-
-# Plot calculated residual entropy with log scaled viscosity, try different scaling
+# Plot calculated residual entropy with log dimless viscosity
 for T in T_values:
     label = f'Temperature {T} K (Ideal Gas)'
-    plt.plot(entro_3[T], dim_less_visc_2(T), 'o', label=label)
+    plt.plot(entropy[T], dim_less_visc(T), 'o', label=label)
 
 # Add labels and a legend
-plt.xlabel(r'Residual entropy [J·mol$^{-1}$·K$^{-1}$]')
-plt.ylabel(r'$\eta$/$\eta_0$')
-plt.title('Argon')
+plt.xlabel(r'S$_{res}\,$ [ J$\cdot$mol$^{-1}\cdot$K$^{-1}$]')
+plt.ylabel(r'$\eta^*$')
+plt.title(r'Dimensionless viscosity ,$\eta^*$, plotted against residual entropy, S$_{res}$, for Argon')
 plt.legend()
 #plt.ylim(0, 1e-7)
 
