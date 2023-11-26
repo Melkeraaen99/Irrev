e553f25c856b9037ebfbf98d336be2fbb6c487f4
Thomas <thomawm@stud.ntnu.no>
2023-11-26 17:46:46 +0100

    Labeled the phase envelope plot

--- phaseenvelope.py
+++ phaseenvelope.py
@@ -5,10 +5,14 @@ import numpy as np
 T_twop, P_twop, V_twop = Initialization.eos.get_envelope_twophase(1e5, Initialization.z, calc_v=True)
 
 
-plt.plot(1/V_twop, T_twop)
+plt.plot(1/V_twop, T_twop, label='Phase Envelope')
 for T in Initialization.T_values:
-    plt.plot(np.array(Initialization.density[T])/Initialization.Avogadro, T*np.ones_like(Initialization.density[T]))
-# Lables etc
+    label = f'Temperature {T:.2f}'
+    plt.plot(np.array(Initialization.density[T])/Initialization.Avogadro, T*np.ones_like(Initialization.density[T]), label=label)
+plt.xlabel('Density')
+plt.ylabel('T [K]')
+plt.title('Phase Envelope')
+plt.legend()
 plt.savefig("Plots/PhaseEnvelope.png")
 
 
